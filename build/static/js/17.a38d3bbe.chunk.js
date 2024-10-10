0012efac4db10754e74be79e0eb8728126307b96
2412pooja1 <pdalvi0203@gmail.com>
2024-10-10 14:02:08 +0530

    changes

---
+++ build/static/js/17.a38d3bbe.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[17],{1063:function(a,s,d){}}]);
+//# sourceMappingURL=17.a38d3bbe.chunk.js.map
